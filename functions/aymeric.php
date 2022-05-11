b8e441c3ee16f1c0473f11ec1f578e38e298f2c0
GobertAymeric <aymeric.gobert@outlook.fr>
2022-05-11 17:06:39 +0200

    Terminer + Bonus

--- functions/aymeric.php
+++ functions/aymeric.php
@@ -3,6 +3,7 @@
 namespace aymeric;
 
 function checkPassword($password) {
+    // head
     echo '<h1 class="text-center">Aymeric</h1>';
     echo "<h5>Mot de passe : $password </h5>";
     echo '<h5 class=text-end>Force du mot de passe</h5>';
@@ -22,41 +23,42 @@ function checkPassword($password) {
 
     $all = array_filter($table);
 
-    if ($table){
-        if (count($all) == 1){
+    if ($table){    
+        if (count($all) == 1){  //Si le password contient 1 condition validé
             echo '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" style="width: 20%" aria valuenow="20" aria-valuemin="0" aria-valuemax="100"></div></div>';
 
         }   
 
-        elseif (count($all) == 2){
+        elseif (count($all) == 2){  //Si le password contient 2 conditions validé
             echo '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: 40%" aria valuenow="40" aria-valuemin="0" aria-valuemax="100"></div></div>';
 
         }
 
-        elseif (count($all) == 3){
+        elseif (count($all) == 3){  //Si le password contient 3 conditions validé
             echo '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: 60%" aria valuenow="60" aria-valuemin="0" aria-valuemax="100"></div></div>';
 
         }
 
-        elseif (count($all) == 4){
+        elseif (count($all) == 4){  //Si le password contient 4 conditions validé
             echo '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%" aria valuenow="80" aria-valuemin="0" aria-valuemax="100"></div></div>';
 
         }
 
-        elseif (count($all) == 5){
+        elseif (count($all) == 5){  // Si le password contient 5 conditions validé
             echo '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style="width: 100%" aria valuenow="100" aria-valuemin="0" aria-valuemax="100"></div></div>';
 
         }
     
-        elseif (count($all) == 0){
+        elseif (count($all) == 0){ // Si le password est vide
             echo '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 0%" aria valuenow="0" aria-valuemin="0" aria-valuemax="100"></div></div>';
 
         } 
-    }
+    }   
     if(!$chiffre || !$minuscule || !$majuscule || !$special || !$caractere ) {
-            
+        
+            //liste des conditions
         if($chiffre || $minuscule || $majuscule || $special || !$caractere ) {
-            echo '<ul class="list-group position-absolute top-50 start-50 translate-middle w-15">
+            echo '<ul class="list-group mt-5 mx-auto" style="max-width: 350px">
                 <li class="list-group-item active" aria-current="true">Le mot de passe doit contenir au moins:</li>';
         }
         
@@ -82,7 +84,7 @@ function checkPassword($password) {
         echo '</ul>';
     } 
 
-    else {
+     else { //Boutton OK
         echo '<a href="index.php" class="text-center btn btn-success position-absolute top-50 start-50 translate-middle py-2 px-3">OK</a>';
     }    
 }
--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 
     </head>
     <body>
+        <button type="button" class="btn btn-primary d-grid gap-1 col-1 mt-5 mx-auto" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Tester votre mot de passe</button>
 
         <div class="container py-3">
 
@@ -38,14 +40,33 @@
                     <p>Pour celà, il vous suffit de le renseigner directement dans l'URL comme dans l'example ci-dessous :</p>
                     <a href="index.php?password=motdepasse" class="link-primary mb-0">index.php?password=motdepasse</a>
                 </div>
-
                 <?php
             }
-
             ?>
-
         </div>
 
+            <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Mot de passe</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="../index.php" method="get">
+                        <label class="form-label mt-4" for="password"style="vertical-align: inherit">Tester votre mot de passe</label>
+                        <input class="form-control" id="password" name="password" type="text" placeholder="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                        <button type="submit" class="btn btn-primary">Tester</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <!-- JS -->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
